904b30a2897ef4a55cd9d391827bc5f2af665ebc
0xdeadb01 <0xdeadb01@protonmail.com>
2021-05-16 20:22:26 -0300

    Copy cleanup.

--- src/components/Crowdsale/Crowdsale.js
+++ src/components/Crowdsale/Crowdsale.js
@@ -12,19 +12,32 @@ import { useTranslation } from 'react-i18next';
 import { TextButton } from 'components/Button/Button';
 import ERC20 from './ERC20.json';
 
+const Address= {
+  Yogi: '0x56bAb4AB4fbF2Fc35A5c4920F48388881acCB247',
+  Crowdsale: '0x56bAb4AB4fbF2Fc35A5c4920F48388881acCB247'
+}
+
+const ExplorerLink = (address, label) => {
+  return (
+    <TextButton
+      href={`https://bscscan.com/address/${address}}`}
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      {label}
+    </TextButton>
+  )
+}
+
 const Crowdsale = () => {
   const { t } = useTranslation();
   const [yogiRemaining, setYogiRemaining] = useState('0');
 
   useEffect(() => {
     const main = async () => {
-      const provider = new ethers.providers.JsonRpcProvider(
-        'https://bsc-dataseed.binance.org/'
-      );
-      const yogiAddr = '0x56bAb4AB4fbF2Fc35A5c4920F48388881acCB247';
-      const crowdsaleAddr = '0x56bAb4AB4fbF2Fc35A5c4920F48388881acCB247';
-      const yogi = await new ethers.Contract(yogiAddr, ERC20, provider);
-      const remaining = await yogi.balanceOf(crowdsaleAddr);
+      const provider = new ethers.providers.JsonRpcProvider('https://bsc-dataseed.binance.org/');
+      const yogi = await new ethers.Contract(Address.Yogi, ERC20, provider);
+      const remaining = await yogi.balanceOf(Address.Crowdsale);
       setYogiRemaining(
         Number(ethers.utils.formatEther(remaining.toString())).toFixed(2)
       );
@@ -38,32 +51,18 @@ const Crowdsale = () => {
       <CrowdsaleContent>
         <CrowdsaleTitle>{t('Crowdsale-Title')}</CrowdsaleTitle>
         <CrowdsaleInstructions>
-          <div>To participate you need to send BNB to the crowdsale contract.</div>
-          <div>You will receive a proportional amount of {' '}
-            <TextButton
-              href="https://bscscan.com/address/0x56bAb4AB4fbF2Fc35A5c4920F48388881acCB247"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              $YOGI
-            </TextButton>
-          </div>
+          <div>{t('Crowdsale-1')}</div>
+          <div>{t('Crowdsale-2')}</div>
         </CrowdsaleInstructions>
 
         <CrowdsaleInstructions>
           <div>Crowdsale address:</div>
-          <TextButton
-            href="https://bscscan.com/address/0x56bAb4AB4fbF2Fc35A5c4920F48388881acCB247"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            0x56bAb4AB4fbF2Fc35A5c4920F48388881acCB247
-          </TextButton>
+          { ExplorerLink(Address.Crowdsale, Address.Crowdsale) }
         </CrowdsaleInstructions>
 
         <CrowdsaleInstructions>
           <div>1 YOGI = 0.001 BNB</div>
-          <div>Remaining YOGI: {yogiRemaining} / 5,000,000</div>
+          <div>{t('Crowdsale-3', { remaining: yogiRemaining, total: "5,000,000" })}</div>
         </CrowdsaleInstructions>
       </CrowdsaleContent>
     </BaseCrowdsale>
