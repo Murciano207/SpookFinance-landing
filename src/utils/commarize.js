ae3eaa978b0e11300510cec61490467e4b035424
jadavasudev <jadavasudev@protonmail.com>
2021-05-19 00:52:47 -0300

    Changed suffix case

--- src/utils/commarize.js
+++ src/utils/commarize.js
@@ -2,7 +2,7 @@ const commarize = (n, p=2) => {
   const order = Math.floor(Math.log10(n) / 3);
   if (order < 0) { return Number(n).toFixed(p); }
 
-  const units = ['', 'k', 'm', 'b', 't'];
+  const units = ['', 'k', 'M', 'B', 'T'];
   const num = n / 1000 ** order;
 
   return `${num.toFixed(p)}${units[order]}`;
